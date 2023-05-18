76d8aae6f9806059958df75c6fa525fbc1cd1819
jorgechvz <cha22060@byui.edu>
2023-05-18 12:46:16 -0500

    Team4 Completed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,43 +1,5 @@
-import { getLocalStorage } from './utils.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 import shoppingCart from './shoppingCart.mjs';
 
-
+loadHeaderFooter();
 shoppingCart();
-
-
-/* function getLenghtCart() {
-  const getCartItems = getLocalStorage('so-cart');
-  const lengthCart = getCartItems.length;
-  return lengthCart;
-} 
-export default function renderLengthCart(selector) {
-  const lengthCart = document.querySelector(selector);
-  lengthCart.innerHTML = getLenghtCart();
-}
-renderLengthCart('.length-cart'); 
-
-// delete item from cart
-function deleteItem() {
-  const deleteBtn = document.querySelector('.deleteBtn');
-  if (deleteBtn) {
-    deleteBtn.addEventListener('click', deleteFunctionality);
-  }
-}
-
-deleteItem();
-
-function deleteFunctionality() {
-  let dataID = this.getAttribute('data-id');
-  const cartItems = getLocalStorage('so-cart');
-  console.log(cartItems);
-  for (let i = 0; i < cartItems.length; i++) {
-    if (cartItems[i].Id == dataID) {
-      cartItems.splice(i, 1);
-      localStorage.removeItem(i);
-      localStorage.setItem('so-cart', JSON.stringify(cartItems));
-    }
-    renderCartContents();
-    return;
-  }
-}
- */
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,5 @@
-/* import renderLengthCart from './cart'; */
 import productList from './productList.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
-/* renderLengthCart('.length-cart'); */
-productList('.product-list', 'tents');
 loadHeaderFooter();
-/* 
-function renderSuperscriptNumberCart(){
-    const lengthRenderCart = document.querySelector('.length-cart');
-    console.log(getLenghtCart());
-    lengthRenderCart.innerHTML = getLenghtCart();
-}
-renderSuperscriptNumberCart(); */
+productList('.product-list', 'tents');
