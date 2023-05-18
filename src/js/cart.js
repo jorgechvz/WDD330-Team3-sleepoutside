d85b47b630039117503ddd31d9e715700746cf8e
Timoteo Tapia <fidel_tapia@byui.edu>
2023-05-18 11:48:40 -0600

    changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,43 +1,44 @@
 import { getLocalStorage } from './utils.mjs';
 import shoppingCart from './shoppingCart.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 
 
 shoppingCart();
+loadHeaderFooter();
 
 
-/* function getLenghtCart() {
-  const getCartItems = getLocalStorage('so-cart');
-  const lengthCart = getCartItems.length;
-  return lengthCart;
-} 
-export default function renderLengthCart(selector) {
-  const lengthCart = document.querySelector(selector);
-  lengthCart.innerHTML = getLenghtCart();
-}
-renderLengthCart('.length-cart'); 
+// function getLenghtCart() {
+//   const getCartItems = getLocalStorage('so-cart');
+//   const lengthCart = getCartItems.length;
+//   return lengthCart;
+// }
+// export default function renderLengthCart(selector) {
+//   const lengthCart = document.querySelector(selector);
+//   lengthCart.innerHTML = getLenghtCart();
+// }
+// renderLengthCart('.length-cart');
 
-// delete item from cart
-function deleteItem() {
-  const deleteBtn = document.querySelector('.deleteBtn');
-  if (deleteBtn) {
-    deleteBtn.addEventListener('click', deleteFunctionality);
-  }
-}
+// // delete item from cart
+// function deleteItem() {
+//   const deleteBtn = document.querySelector('.deleteBtn');
+//   if (deleteBtn) {
+//     deleteBtn.addEventListener('click', deleteFunctionality);
+//   }
+// }
 
-deleteItem();
+// deleteItem();
 
-function deleteFunctionality() {
-  let dataID = this.getAttribute('data-id');
-  const cartItems = getLocalStorage('so-cart');
-  console.log(cartItems);
-  for (let i = 0; i < cartItems.length; i++) {
-    if (cartItems[i].Id == dataID) {
-      cartItems.splice(i, 1);
-      localStorage.removeItem(i);
-      localStorage.setItem('so-cart', JSON.stringify(cartItems));
-    }
-    renderCartContents();
-    return;
-  }
-}
- */
+// function deleteFunctionality() {
+//   let dataID = this.getAttribute('data-id');
+//   const cartItems = getLocalStorage('so-cart');
+//   console.log(cartItems);
+//   for (let i = 0; i < cartItems.length; i++) {
+//     if (cartItems[i].Id == dataID) {
+//       cartItems.splice(i, 1);
+//       localStorage.removeItem(i);
+//       localStorage.setItem('so-cart', JSON.stringify(cartItems));
+//     }
+//     renderCartContents();
+//     return;
+//   }
+// }
