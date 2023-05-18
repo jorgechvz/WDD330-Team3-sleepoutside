8c06877aa27ec02f8eeddab84886f914492c4604
jorgechvz <cha22060@byui.edu>
2023-05-18 13:06:55 -0500

    Delete Product js comment

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,3 @@ import productDetails from './productDetails.mjs';
 const idProduct = getParam('product');
 productDetails(idProduct);
 
-
-// remove item from cart
-// document.querySelector('deleteBtn').addEventListener('click', () => {
-//   console.log('button clicked');
-// })
